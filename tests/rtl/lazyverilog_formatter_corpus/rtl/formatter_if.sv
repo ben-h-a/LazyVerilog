71b3b3b776d19f8b7af2f4d49ef282ae7dc2ba59
kjoonha <g1004jay@snu.ac.kr>
2026-05-30 03:38:50 +0900

    remove unused files

--- tests/rtl/lazyverilog_formatter_corpus/rtl/formatter_if.sv
+++
@@ -1,7 +0,0 @@
-interface formatter_if #(parameter int Width = `FORMATTER_WIDTH) (input logic clk);
-    logic [Width-1:0] req_data;
-    logic req_valid;
-    logic req_ready;
-
-    modport host(input clk, output req_data, output req_valid, input req_ready), device(input clk, input req_data, input req_valid, output req_ready);
-endinterface
