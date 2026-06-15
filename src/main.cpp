e2ae2fef7c7eb695bc90f18e5bfd17add14f6030
kjoonha <g1004jay@snu.ac.kr>
2026-06-15 18:24:49 +0900

    Fix Windows LSP stdio framing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include "server.hpp"
 
+#ifdef _WIN32
+#include <fcntl.h>
+#include <io.h>
+#endif
+
 int main() {
+#ifdef _WIN32
+    _setmode(_fileno(stdin), _O_BINARY);
+    _setmode(_fileno(stdout), _O_BINARY);
+#endif
+
     LazyVerilogServer server;
     server.run();
     return 0;
